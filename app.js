6144464e991529a4c5bfaf18eeaf041091b5a194
galiphasandugan <galiphasandugan@gmail.com>
2023-06-21 12:07:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const API_KEY = "fe6d890f46834d6b9d94e6a07cd27c43"
+
+const allBtns  = document.querySelectorAll(".btn")
+const container = document.querySelector(".container")
+
+allBtns.forEach((btn)=>{
+
+    btn.addEventListener("click",(e)=>{
+
+        console.log(e.target.innerText)
+        getData(e.target.innerText)
+    })
+})
+
+const getData = async(subject) =>{
+
+    const response = await fetch("https://newsapi.org/v2/everything?q={subject}&apiKey=fe6d890f46834d6b9d94e6a07cd27c43")
+
+    const data = await response.json()
+    console.log(data.articles)
+
+
+}
+
+
